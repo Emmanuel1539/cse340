aca3ea914ea4c6e5144c2f3658f57ed1d0a6be8a
Emmanuel1539 <ekenedonzube1539@gmail.com>
2024-10-09 05:20:49 +0100

    updated assignment 2

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -1,3 +1,5 @@
+
+--Insert data to the account table
 INSERT INTO public.account (
     account_firstname, account_lastname,
     account_email, account_password
@@ -7,28 +9,28 @@ VALUES (
 );
 
 
-
+--Update the account table 
 UPDATE public.account
 SET account_type = 'Admin'
 WHERE account_email = 'tony@starkent.com';
 
-
+--Delete data from account table
 DELETE FROM public.account
 WHERE account_email = 'tony@starkent.com';
 
-
+--Update the inventory table
 UPDATE public.INVENTORY
 SET inv_description = REPLACE(inv_description, 'small interiors','a huge interiors')
 WHERE (inv_make = 'GM' AND inv_model = 'Hummer');
 
-
+-- Joining inventory and classification tables
 SELECT inv.inv_make, inv.inv_model, clas.classification_name
 FROM public.inventory inv
 INNER JOIN public.classification clas
 ON inv.classification_id = clas.classification_id
 WHERE clas.classification_name = 'Sport';
 
-
+--Updating the inventory table
 UPDATE public.INVENTORY
 SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
     inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
