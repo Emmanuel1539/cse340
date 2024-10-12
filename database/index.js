cc3430d9495a1b9c8588d083fb0b99dbefe56faf
Emmanuel1539 <ekenedonzube1539@gmail.com>
2024-10-12 12:39:05 +0100

    assignment 2 complete

--- database/index.js
+++ database/index.js
@@ -15,7 +15,28 @@ if (process.env.NODE_ENV == "development") {
         connectionString: process.env.DATABASE_URL,
         ssl: {
             rejectUnauthorized: false,
-        }
+        },
 
     })
+
+    // Added for troubleshooting queries
+    // during development
+    module.exports = {
+        async query(text, params){
+            try{
+                const res = await pool.query(text, params)
+                console.log('executed query',{text})
+                return res
+            } catch(error){
+                console.error('error in querry',{text})
+                throw error
+            }
+        },
+    }
+
+} else{
+    pool = new Pool({
+        connectionString: process.env.DATABASE_URL,
+    })
+    module.exports = pool
 }
